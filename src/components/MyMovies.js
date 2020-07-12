a47b4b64a7cbe1c0fd0c7b7a26ceb2d07ff1dd18
Adam Gardner <adam.gardner4991@gmail.com>
2020-07-12 16:52:15 +0100

    MyMovies component created

---
+++ src/components/MyMovies.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Header from '../Header';
+import Button from '../Button';
+import MovieTable from '../MovieTable';
+import MovieFormDialogue from '../Dialogues/MovieFormDialogue';
+import Container from '@material-ui/core/Container';
+
+const MyMovies = () => {
+    const [open, setOpen] = React.useState(false);
+
+    // handles opening the add movie form dialogue
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    // handles closing the add movie form dialogue
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <div className="background-image">
+            <Container width="70%">
+                <Header>My Movies</Header>
+                <MovieFormDialogue handleClose={handleClose} openDialogue={open} />
+                <div className="center">
+                    <Button
+                        className="btn btn-secondary"
+                        onClick={handleClickOpen}
+                    >Add a movie
+                    </Button>
+                </div>
+                <MovieTable />
+            </Container>
+        </div>
+    );
+};
+
+export default MyMovies;
