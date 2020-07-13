2e24b4b954f109a175428c2974115ffaba73f3c2
Adam Gardner <adam.gardner4991@gmail.com>
2020-07-13 12:58:14 +0100

    Alert dialogue component created, used if user has not input a  movie title

---
+++ src/components/Dialogues/AlertDialogue.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Button from '../Button';
+import Alert from '@material-ui/lab/Alert';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+
+const AlertDialogue = ({ open, handleClose, dialogueHeading, dialogueContent }) => {
+    const noMarginTop = {
+        marginTop: "0",
+    }
+    return (
+        <Dialog
+            open={open}
+            onClose={handleClose}
+        >
+            <h1 className="dialogue-title">{dialogueHeading}</h1>
+            <DialogContent>
+                <DialogContentText>
+                    <Alert severity="warning">{dialogueContent}</Alert>
+                </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+                <Button furtherStyling={noMarginTop} onClick={handleClose}>Ok</Button>
+            </DialogActions>
+        </Dialog>
+    );
+};
+
+export default AlertDialogue;
