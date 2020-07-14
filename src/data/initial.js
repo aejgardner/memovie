bc5ab297c495b216c7cacd30031c0f857d715856
Adam Gardner <adam.gardner4991@gmail.com>
2020-07-14 01:36:28 +0100

    landing page made responsive

--- src/data/initial.js
+++ src/data/initial.js
@@ -1,27 +1,5 @@
 const initial = {
-    movies: [
-        {
-            movieTitle: "The House That Jack Built",
-            movieGenre: "Thriller",
-            movieDirector: "Lars Von Trier",
-            movieStarring: ["Matt Dillon", "Uma Therman"],
-            watched: true
-        },
-        {
-            movieTitle: "Mother!",
-            movieGenre: "Thriller",
-            movieDirector: "Darren Aronofsky",
-            movieStarring: ["Jennifer Lawrence"],
-            watched: true
-        },
-        {
-            movieTitle: "Hot Fuzz",
-            movieGenre: "Comedy",
-            movieDirector: "Edgar Wright",
-            movieStarring: ["Simon Pegg"],
-            watched: true
-        }
-    ],
+    movies: [],
     pickedMovie: {}
 }
 
