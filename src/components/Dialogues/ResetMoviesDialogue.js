6197d0a38608e6add1264e4aeb1850841a57fe0f
Adam Gardner <adam.gardner4991@gmail.com>
2020-07-18 22:53:25 +0100

    inline styling replced with material ui's makestyles hook API in reset movies dialogue

--- src/components/Dialogues/ResetMoviesDialogue.js
+++ src/components/Dialogues/ResetMoviesDialogue.js
@@ -5,43 +5,53 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import ReportProblemOutlinedIcon from '@material-ui/icons/ReportProblemOutlined';
 import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles';
 
-const ResetMoviesDialogue = ({ open, handleClose, handleReset }) => (
-    <Dialog
-        open={open}
-        onClose={handleClose}
-    >
-        <div className="dialogue__container">
-            <h1 className="dialogue__heading">Reset your movies?</h1>
-            <DialogContent style={{ padding: "none" }}>
-                <Paper className="paper"
-                    style={{
-                        backgroundColor: "rgba(255, 191, 102, 0.9)",
-                        marginBottom: "0.5rem",
-                        paddingTop: "2rem",
-                        paddingBottom: "2rem",
-                    }}
-                    elevation={3}>
-                    <div className="flex-align-center dialogue__content-alert">
-                        <ReportProblemOutlinedIcon />
-                        <p>This will clear the table and you will have to start over</p>
+const ResetMoviesDialogue = ({ open, handleClose, handleReset }) => {
+    const useStyles = makeStyles({
+        paper: {
+            backgroundColor: "rgba(255, 191, 102, 0.9)",
+            marginBottom: "0.5rem",
+            paddingTop: "2rem",
+            paddingBottom: "2rem",
+        },
+        paddingNone: {
+            padding: "0"
+        }
+    });
+
+    const classes = useStyles();
+    return (
+        <Dialog
+            open={open}
+            onClose={handleClose}
+        >
+            <div className="dialogue__container">
+                <h1 className="dialogue__heading">Reset your movies?</h1>
+                <DialogContent className={classes.paddingNone}>
+                    <Paper className={classes.paper + " paper"}
+                        elevation={3}>
+                        <div className="flex-align-center dialogue__content-alert">
+                            <ReportProblemOutlinedIcon />
+                            <p>This will clear the table and you will have to start over</p>
+                        </div>
+                    </Paper>
+                </DialogContent>
+                <DialogActions className={classes.paddingNone}>
+                    <div className="center">
+                        <Button
+                            className="btn"
+                            onClick={handleClose}>Back
+                    </Button>
+                        <Button
+                            className="btn"
+                            onClick={handleReset}>Reset
+                    </Button>
                     </div>
-                </Paper>
-            </DialogContent>
-            <DialogActions style={{ padding: "0" }}>
-                <div className="center">
-                    <Button
-                        className="btn"
-                        onClick={handleClose}>Back
-                    </Button>
-                    <Button
-                        className="btn"
-                        onClick={handleReset}>Reset
-                    </Button>
-                </div>
-            </DialogActions>
-        </div>
-    </Dialog>
-);
+                </DialogActions>
+            </div>
+        </Dialog>
+    );
+};
 
 export default ResetMoviesDialogue;
