a4da160a978ba758e23250fd72dd3236c64b4777
Adam Gardner <adam.gardner4991@gmail.com>
2020-07-18 21:25:12 +0100

    material ui paper component added to picked movie dialogue, used as the background for the movie title and director

--- src/components/Dialogues/PickedMovieDialogue/PickedMovieDialogue.js
+++ src/components/Dialogues/PickedMovieDialogue/PickedMovieDialogue.js
@@ -3,6 +3,7 @@ import Button from '../../Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
+import Paper from '@material-ui/core/Paper';
 
 const PickedMovieDialogue = ({ handleClose, open, pickedMovie }) => (
     <Dialog
@@ -11,9 +12,20 @@ const PickedMovieDialogue = ({ handleClose, open, pickedMovie }) => (
     >
         <div className="dialogue__container">
             <h1 className="dialogue__heading">Your movie</h1>
-            <DialogContent style={{ padding: "0" }}>
-                <h1 className="picked-movie__h1">{pickedMovie.movieTitle}</h1>
-                <p className="picked-movie__director">{pickedMovie.movieDirector}</p>
+            <DialogContent >
+                <Paper
+                    className="paper"
+                    style={{
+                        backgroundColor: "#cfe8fc",
+                        marginBottom: "1rem",
+                        paddingTop: "2rem",
+                        paddingBottom: "2rem",
+                    }}
+                    elevation={3}
+                >
+                    <h1 className="picked-movie__h1">{pickedMovie.movieTitle}</h1>
+                    <p className="picked-movie__director">Director: {pickedMovie.movieDirector}</p>
+                </Paper>
             </DialogContent>
             <DialogActions style={{ justifyContent: "center" }}>
                 <Button
