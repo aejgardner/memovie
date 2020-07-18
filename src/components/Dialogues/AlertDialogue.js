2e31a038ff160eb0f2eee3ccec75ca52e41d0712
Adam Gardner <adam.gardner4991@gmail.com>
2020-07-18 21:48:51 +0100

    material ui paper component added to alert dialogue, used as background for content message. Css for alert message altered accordingly

--- src/components/Dialogues/AlertDialogue.js
+++ src/components/Dialogues/AlertDialogue.js
@@ -4,6 +4,7 @@ import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import ReportProblemOutlinedIcon from '@material-ui/icons/ReportProblemOutlined';
+import Paper from '@material-ui/core/Paper';
 
 const AlertDialogue = ({ open, handleClose, dialogueHeading, dialogueContent }) => (
     <Dialog
@@ -13,10 +14,19 @@ const AlertDialogue = ({ open, handleClose, dialogueHeading, dialogueContent }) 
         <div className="dialogue__container">
             <h1 className="dialogue__heading">{dialogueHeading}</h1>
             <DialogContent style={{ padding: "0" }}>
-                <div className="flex-align-center dialogue__content-alert">
-                    <ReportProblemOutlinedIcon />
-                    <p>{dialogueContent}</p>
-                </div>
+                <Paper className="paper"
+                    style={{
+                        backgroundColor: "rgba(255, 191, 102, 0.9)",
+                        marginBottom: "0.5rem",
+                        paddingTop: "2rem",
+                        paddingBottom: "2rem",
+                    }}
+                    elevation={3}>
+                    <div className="flex-align-center dialogue__content-alert">
+                        <ReportProblemOutlinedIcon />
+                        <p>{dialogueContent}</p>
+                    </div>
+                </Paper>
             </DialogContent>
             <DialogActions style={{ padding: "0" }}>
                 <Button className="btn" onClick={handleClose}>Ok</Button>
