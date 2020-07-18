02cfae326678d2f22bd442385c19973e56da78bb
Adam Gardner <adam.gardner4991@gmail.com>
2020-07-18 21:43:51 +0100

    material ui paper component added to no movies dialogue, used as background for content message. Css for no movies message altered accordingly

--- src/components/Dialogues/NoMoviesDialogue.js
+++ src/components/Dialogues/NoMoviesDialogue.js
@@ -3,6 +3,7 @@ import Button from '../Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
+import Paper from '@material-ui/core/Paper';
 
 const NoMoviesDialogue = ({ handleClose, open }) => (
     <Dialog
@@ -12,7 +13,16 @@ const NoMoviesDialogue = ({ handleClose, open }) => (
         <div className="dialogue__container">
             <h1 className="dialogue__heading">No movies!</h1>
             <DialogContent style={{ padding: "0" }}>
-                <h3 className="no-movies__h3">There are no movies matching your search criteria. Please change your filters or add a movie that matches them.</h3>
+                <Paper className="paper"
+                    style={{
+                        backgroundColor: "#cfe8fc",
+                        marginBottom: "1rem",
+                        paddingTop: "2rem",
+                        paddingBottom: "2rem",
+                    }}
+                    elevation={3}>
+                    <h3 className="no-movies__h3">There are no movies matching your search criteria. Please change your filters or add a movie that matches them.</h3>
+                </Paper>
             </DialogContent>
             <DialogActions style={{ padding: "0" }}>
                 <Button
