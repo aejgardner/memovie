93b707eb86aeacc2813620020a6002f81044dfad
Adam Gardner <adam.gardner4991@gmail.com>
2020-07-19 20:17:21 +0100

    movie form input component refactored, inline styling replaced with material uis makestyles hook

--- src/components/MovieFormInput.js
+++ src/components/MovieFormInput.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { FormControl, InputLabel, Input } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
 const MovieFormInput = ({ value, label, onChange, id, faIcon }) => {
+    // mui's usestyles hook, allows for custom classnames in material ui components
+    const useStyles = makeStyles({
+        marginBottom: {
+            marginBottom: "1rem"
+        }
+    });
+
+    // storing custom classes in classes variable
+    const classes = useStyles();
     return (
         <FormControl>
             <InputLabel htmlFor={id}>{label}<i className={faIcon}></i></InputLabel>
             <Input
-                style={{ marginBottom: "1rem" }}
+                className={classes.marginBottom}
                 id={id}
                 value={value}
                 onChange={onChange}
