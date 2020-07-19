16cdf57d71144a40d98f119321e2ff2fa43a6ca0
Adam Gardner <adam.gardner4991@gmail.com>
2020-07-19 20:07:45 +0100

    filter group component refactored, inline styling replaced with material uis makestyles hook

--- src/components/FilterGroup.js
+++ src/components/FilterGroup.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Paper from '@material-ui/core/Paper';
 import Collapse from '@material-ui/core/Collapse';
+import { makeStyles } from '@material-ui/core/styles';
 
 const FilterGroup = ({ heading, filtersNoDuplicates, onChange }) => {
     const [showFilters, setShowFilters] = useState(false)
@@ -13,33 +14,52 @@ const FilterGroup = ({ heading, filtersNoDuplicates, onChange }) => {
         setRotateChevron(!rotateChevron);
     }
 
-    // rotates chevron 180 degrees when clicked to show or hide filters
-    const rotate = rotateChevron ? "rotate(180deg)" : "rotate(0)"
+    // mui's usestyles hook, allows for custom classnames in material ui components
+    const useStyles = makeStyles({
+        paper: {
+            padding: "1rem"
+        },
+        roboto: {
+            fontFamily: "Roboto, sans-serif"
+        },
+        rotateChev: {
+            transform: rotateChevron ? "rotate(180deg)" : "rotate(0)",
+            transition: "all 0.2s linear"
+        },
+        flexColumn: {
+            flexDirection: "column"
+        },
+        marginLeft: {
+            marginLeft: "0.3rem"
+        }
+    });
+
+    // storing custom classes in classes variable
+    const classes = useStyles();
 
     return (
         <div className="filters__group">
             <div className="filters__heading flex-align-center center">
                 <h4>{heading}</h4>
                 <ExpandMoreIcon
-                    className="filters__chevron"
-                    style={{ transform: rotate, transition: "all 0.2s linear" }}
+                    className={classes.rotateChev + " filters__chevron"}
                     onClick={handleFilters}
                 />
             </div>
             {filtersNoDuplicates.length ?
                 <Collapse in={showFilters}>
-                    <Paper elevation={4} style={{ padding: "1rem" }}>
-                        <div className="flex-align-center center" style={{ flexDirection: "column" }}>
+                    <Paper elevation={4} className={classes.paper}>
+                        <div className={classes.flexColumn + " flex-align-center center"}>
                             {filtersNoDuplicates.map((filter, i) => {
                                 return (
                                     <div key={i} className="flex-align-center mb-1">
-                                        <label style={{ fontFamily: "Roboto, sans-serif" }} htmlFor="filter">{filter}</label>
+                                        <label className={classes.fontFamily} htmlFor="filter">{filter}</label>
                                         <input
                                             id="filter"
                                             type="checkbox"
                                             value={filter}
                                             onChange={onChange}
-                                            style={{ marginLeft: "0.3rem" }}
+                                            className={classes.marginLeft}
                                         />
                                     </div>
                                 )
