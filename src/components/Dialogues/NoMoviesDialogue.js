aa450cc6fa15de0fe26960c6cc568f7d402dc74b
Adam Gardner <adam.gardner4991@gmail.com>
2020-07-19 19:41:16 +0100

    no movies dialogue has been refactored so that inline styling has been replaced with material ui's makestyles hook

--- src/components/Dialogues/NoMoviesDialogue.js
+++ src/components/Dialogues/NoMoviesDialogue.js
@@ -4,34 +4,46 @@ import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles';
 
-const NoMoviesDialogue = ({ handleClose, open }) => (
-    <Dialog
-        open={open}
-        onClose={handleClose}
-    >
-        <div className="dialogue__container">
-            <h1 className="dialogue__heading">No movies!</h1>
-            <DialogContent style={{ padding: "0" }}>
-                <Paper className="paper"
-                    style={{
-                        backgroundColor: "#cfe8fc",
-                        marginBottom: "0.5rem",
-                        paddingTop: "2rem",
-                        paddingBottom: "2rem",
-                    }}
-                    elevation={3}>
-                    <h3 className="no-movies__h3">There are no movies matching your search criteria. Please change your filters or add a movie that matches them.</h3>
-                </Paper>
-            </DialogContent>
-            <DialogActions style={{ padding: "0" }}>
-                <Button
-                    className="btn"
-                    onClick={handleClose}>Back
+const NoMoviesDialogue = ({ handleClose, open }) => {
+    const useStyles = makeStyles({
+        paper: {
+            backgroundColor: "#cfe8fc",
+            marginBottom: "0.5rem",
+            paddingTop: "2rem",
+            paddingBottom: "2rem",
+        },
+        paddingNone: {
+            padding: "0"
+        }
+    });
+
+    const classes = useStyles();
+    return (
+        <Dialog
+            open={open}
+            onClose={handleClose}
+        >
+            <div className="dialogue__container">
+                <h1 className="dialogue__heading">No movies!</h1>
+                <DialogContent className={classes.paddingNone}>
+                    <Paper
+                        className={classes.paper + " paper"}
+                        elevation={3}
+                    >
+                        <h3 className="no-movies__h3">There are no movies matching your search criteria. Please change your filters or add a movie that matches them.</h3>
+                    </Paper>
+                </DialogContent>
+                <DialogActions className={classes.paddingNone}>
+                    <Button
+                        className="btn"
+                        onClick={handleClose}>Back
                 </Button>
-            </DialogActions>
-        </div>
-    </Dialog>
-);
+                </DialogActions>
+            </div>
+        </Dialog>
+    );
+};
 
 export default NoMoviesDialogue;
