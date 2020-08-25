e1fd44f6b1e440bccf9d437d6f7ba3af6f73beb3
Adam Gardner <adam.gardner4991@gmail.com>
2020-08-25 19:29:23 +0100

    random filtered movie function updated to use moviecast property

--- src/functions/randomFilteredMovie.js
+++ src/functions/randomFilteredMovie.js
@@ -1,11 +1,11 @@
 export const randomFilteredMovie = (filters, movies) => {
 
-    // this reducer checks if at least one value of each movie object is included in the filters array. The movieStarring property of each movie, initially a string, is split into an array which can then also be looped through. If a movie's genre or director or is in the filters array, or if the movie contains a star that is in there, then the movie object is added to the accumulator array.
+    // this reducer checks if at least one value of each movie object is included in the filters array. The movieCast property of each movie, initially a string, is split into an array which can then also be looped through. If a movie's genre or director or is in the filters array, or if the movie contains a star that is in there, then the movie object is added to the accumulator array.
     let filteredMovies = movies.reduce((acc, movie) => {
         if (
             Object.values(movie).some(val => filters.includes(val))
             ||
-            movie.movieStarring.split(', ').some(star => filters.includes(star))
+            movie.movieCast.split(', ').some(star => filters.includes(star))
         ) {
             return [...acc, movie]
         }
