e399aa248c369f26b9eae4af0044cfc80bd15a87
Adam Gardner <adam.gardner4991@gmail.com>
2020-08-25 18:58:01 +0100

    slash added to local base url in axios file

--- src/data/axios.js
+++ src/data/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://homestead.test/api",
+    baseURL: "http://homestead.test/api/",
     headers: {
         Accept: "application/json",
     },
