bf69bc9f91232a71b824a1e1be6a6e7c61bb861f
Adam Gardner <adam.gardner4991@gmail.com>
2020-08-25 09:48:01 +0100

    useSelector hook removed from private route component as it's no longer necessary

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom'
-import { useSelector } from 'react-redux';
 
 export const PrivateRoute = ({ component: Component, ...rest }) => {
-    const user = useSelector(state => state.user)
     const token = localStorage.getItem('user')
     return (
         <Route {...rest} render={props => (
