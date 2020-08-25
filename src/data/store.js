9b2574dcd22fe5c3de9c179fb5ed75b1c4dcf2ae
Adam Gardner <adam.gardner4991@gmail.com>
2020-08-25 18:58:57 +0100

    thunk added to redux store and alternate store with persist state added

--- src/data/store.js
+++ src/data/store.js
@@ -1,15 +1,23 @@
-import { createStore, compose } from "redux";
-import reducer from "./reducer";
+// import RootReducer from "./reducers/RootReducer";
+import reducer from "./reducers/reducer";
 import initial from "./initial";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
 import persistState from "redux-localstorage";
 
 const composeEnhancers =
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+// const store = createStore(
+//     reducer,
+//     initial,
+//     composeEnhancers(applyMiddleware(thunk), persistState()),
+// );
+
 const store = createStore(
     reducer,
     initial,
-    composeEnhancers(persistState())
+    composeEnhancers(applyMiddleware(thunk))
 );
 
 export default store;
