e6b8eab8c4e8872bdef02d3df95cedb171dddd0b
Adam Gardner <adam.gardner4991@gmail.com>
2020-08-29 20:13:43 +0100

    new login form in progress

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,9 +4,9 @@ import Login from './Login';
 import Register from './Register';
 
 const home = () => (
-    <div className="authform__container home-image">
-        <h1 className="authform__h1">Welcome to MeMovie!</h1>
-        <h2 className="authform__sub-heading">Your personalised movie hub</h2>
+    <div className="authform__container background-image">
+        {/* <h1 className="authform__h1">Welcome to MeMovie!</h1>
+        <h2 className="authform__sub-heading">Your personalised movie hub</h2> */}
         <Switch>
             <Route exact path="/home/register" component={Register} />
             <Route path="/home" component={Login} />
--- src/components/Home/Login.js
+++ src/components/Home/Login.js
@@ -64,45 +64,72 @@ const Login = ({ history }) => {
     const classes = useStyles();
 
     return (
-        <Paper
-            className={classes.paper + " paper authform__paper center"}
-            elevation={3}
-        >
-            <h1 className={classes.mb}>Login Here</h1>
 
-            <form autoComplete="off" onSubmit={handleSubmit}>
-                <TextField
-                    id="email"
-                    label="Email"
-                    className={classes.mb}
-                    placeholder="Enter your email"
-                    fullWidth
-                    margin="normal"
-                    variant="outlined"
-                    required
-                    type="email"
-                    onChange={handleChange}
-                />
-                <TextField
-                    id="password"
-                    label="Password"
-                    className={classes.mb}
-                    placeholder="Enter your password"
-                    fullWidth
-                    margin="normal"
-                    variant="outlined"
-                    type="password"
-                    required
-                    onChange={handleChange}
-                />
-                <Button className="btn mb-1" type="submit">
-                    Login
-                    </Button>
+        <div class="app">
+
+            <div class="bg"></div>
+
+            <form>
+                <header>
+                    <img src="https://assets.codepen.io/3931482/internal/avatars/users/default.png?format=auto&height=80&version=1592223909&width=80" />
+                </header>
+
+                <div class="inputs">
+                    <input type="text" name="" placeholder="username or email" />
+                    <input type="password" name="" placeholder="password" />
+
+                    <p class="light"><a href="#">Forgot password?</a></p>
+                </div>
+
             </form>
-            <b className="mb-1">{authResponse !== null && authResponse}</b>
-            <p>New to MeMovie? Register <strong><Link to="/home/register">here</Link></strong></p>
-        </Paper>
+
+            <footer>
+                <button>Continue</button>
+                <p>Don't have an account? <a href="#">Sign Up</a></p>
+            </footer>
+
+
+        </div>
     );
 };
 
 export default Login;
+
+// <Paper
+        //     className={classes.paper + " paper authform__paper center"}
+        //     elevation={3}
+        // >
+        //     <h1 className={classes.mb}>Login Here</h1>
+
+        //     <form autoComplete="off" onSubmit={handleSubmit}>
+        //         <TextField
+        //             id="email"
+        //             label="Email"
+        //             className={classes.mb}
+        //             placeholder="Enter your email"
+        //             fullWidth
+        //             margin="normal"
+        //             variant="outlined"
+        //             required
+        //             type="email"
+        //             onChange={handleChange}
+        //         />
+        //         <TextField
+        //             id="password"
+        //             label="Password"
+        //             className={classes.mb}
+        //             placeholder="Enter your password"
+        //             fullWidth
+        //             margin="normal"
+        //             variant="outlined"
+        //             type="password"
+        //             required
+        //             onChange={handleChange}
+        //         />
+        //         <Button className="btn mb-1" type="submit">
+        //             Login
+        //             </Button>
+        //     </form>
+        //     <b className="mb-1">{authResponse !== null && authResponse}</b>
+        //     <p>New to MeMovie? Register <strong><Link to="/home/register">here</Link></strong></p>
+        // </Paper>
