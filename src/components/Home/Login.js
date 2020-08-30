b89aac4a30a469eb1c8c3fbe4300c6a38d995041
Adam Gardner <adam.gardner4991@gmail.com>
2020-08-30 10:48:08 +0100

    login form given new card design

--- src/components/Home/Login.js
+++ src/components/Home/Login.js
@@ -1,12 +1,8 @@
 import React, { useReducer, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import { resetAuthResponsePerComponent } from '../../data/actions/state';
+import { resetAuthResponse } from '../../data/actions/state';
 import { loginUser } from '../../data/actions/api';
-import TextField from '@material-ui/core/TextField';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Button from '../Button';
 
 const initialState = {
     email: "",
@@ -32,7 +28,7 @@ const Login = ({ history }) => {
     const authResponse = useSelector(state => state.auth.authResponse)
 
     useEffect(() => {
-        dispatchAction(resetAuthResponsePerComponent());
+        dispatchAction(resetAuthResponse());
     }, []);
 
     const handleSubmit = (e) => {
@@ -50,86 +46,43 @@ const Login = ({ history }) => {
         })
     }
 
-    // mui's makestyles hook, allows for custom classnames in material ui components
-    const useStyles = makeStyles({
-        mb: {
-            marginBottom: "1.5rem"
-        },
-        paper: {
-            backgroundColor: "rgb(114, 215, 255)",
-        }
-    });
-
-    // storing custom classes in classes variable
-    const classes = useStyles();
-
     return (
-
-        <div class="app">
-
-            <div class="bg"></div>
-
-            <form>
-                <header>
-                    <img src="https://assets.codepen.io/3931482/internal/avatars/users/default.png?format=auto&height=80&version=1592223909&width=80" />
+        <div className="authform">
+            <div className="authform__background-blue"></div>
+            <div className="authform__contents-login mb-1">
+                <header className="authform__header-login">
+                    <h1 className="authform__h1-login"> <span className="authform__h1-me">Me</span>Movie </h1>
+                    <h3 className="authform__sub-heading-login">Your personalised movie hub</h3>
+                    <h3 className="authform__login-h3">Login</h3>
                 </header>
-
-                <div class="inputs">
-                    <input type="text" name="" placeholder="username or email" />
-                    <input type="password" name="" placeholder="password" />
-
-                    <p class="light"><a href="#">Forgot password?</a></p>
-                </div>
-
-            </form>
-
-            <footer>
-                <button>Continue</button>
-                <p>Don't have an account? <a href="#">Sign Up</a></p>
+                <form onSubmit={handleSubmit}>
+                    <div className="authform__inputs-container-login">
+                        <input
+                            onChange={handleChange}
+                            id="email" type="text"
+                            placeholder="email"
+                        />
+                        <input
+                            onChange={handleChange}
+                            id="password"
+                            type="password"
+                            placeholder="password"
+                        />
+                    </div>
+                    <button type="submit" className={"authform__btn" + (authResponse ? " authform__btn-mb" : "")}>Login</button>
+                </form>
+            </div>
+            <footer className="authform__footer">
+                {authResponse !== null ?
+                    (<p className="authform__message">{authResponse}</p>)
+                    :
+                    null
+                }
+                <p className="authform__switch-form">New to MeMovie? Register <strong ><Link
+                    className="authform__switch-form-red" to="/home/register">here</Link></strong></p>
             </footer>
-
-
         </div>
     );
 };
 
 export default Login;
-
-// <Paper
-        //     className={classes.paper + " paper authform__paper center"}
-        //     elevation={3}
-        // >
-        //     <h1 className={classes.mb}>Login Here</h1>
-
-        //     <form autoComplete="off" onSubmit={handleSubmit}>
-        //         <TextField
-        //             id="email"
-        //             label="Email"
-        //             className={classes.mb}
-        //             placeholder="Enter your email"
-        //             fullWidth
-        //             margin="normal"
-        //             variant="outlined"
-        //             required
-        //             type="email"
-        //             onChange={handleChange}
-        //         />
-        //         <TextField
-        //             id="password"
-        //             label="Password"
-        //             className={classes.mb}
-        //             placeholder="Enter your password"
-        //             fullWidth
-        //             margin="normal"
-        //             variant="outlined"
-        //             type="password"
-        //             required
-        //             onChange={handleChange}
-        //         />
-        //         <Button className="btn mb-1" type="submit">
-        //             Login
-        //             </Button>
-        //     </form>
-        //     <b className="mb-1">{authResponse !== null && authResponse}</b>
-        //     <p>New to MeMovie? Register <strong><Link to="/home/register">here</Link></strong></p>
-        // </Paper>
