da1aabf394e73c8eb27255f43d0349e4d0a2c962
Adam Gardner <adam.gardner4991@gmail.com>
2020-08-30 10:42:44 +0100

    welcome message removed from home component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,8 +5,6 @@ import Register from './Register';
 
 const home = () => (
     <div className="authform__container background-image">
-        {/* <h1 className="authform__h1">Welcome to MeMovie!</h1>
-        <h2 className="authform__sub-heading">Your personalised movie hub</h2> */}
         <Switch>
             <Route exact path="/home/register" component={Register} />
             <Route path="/home" component={Login} />
