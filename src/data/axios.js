653e118ac1cc341ef8dcb72449274fe5dd3da7d4
Adam Gardner <adam.gardner4991@gmail.com>
2020-08-30 16:06:03 +0100

    base url updated to use backand route

--- src/data/axios.js
+++ src/data/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://homestead.test/api/",
+    baseURL: "https://afternoon-gorge-82119.herokuapp.com/api",
+    // use baseURL: "http://homestead.test/api" if running locally
     headers: {
         Accept: "application/json",
     },
